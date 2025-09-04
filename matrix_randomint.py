78cafcfb347d1e9f4afc59a25fbb85e322e5fdb8
Amulya-R3008 <159991693+Amulya-R3008@users.noreply.github.com>
2025-09-04 17:23:30 +0530

    Delete matrix_randomint.py

--- matrix_randomint.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-# Create a 5x5 array with random integers between 1 and 100
-arr = np.random.randint(1, 101, size=(5, 5))
-
-print("5x5 Array with random integers between 1 and 100:")
-print(arr)
